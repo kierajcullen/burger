7d0ecf312e1770758584eaae865738d4b0a36aac
kierajcullen <kierajcullen@gmail.com>
2021-03-20 17:28:41 -0400

    new commit

--- config/connection.js
+++ config/connection.js
@@ -3,9 +3,9 @@ var mysql = require("mysql");
 
 var connection = mysql.createConnection({
   host: "localhost",
-  port: 3030,
+  port: 3306,
   user: "root",
-  password: "Xptklmnr102*",
+  password: "",
   database: "burgers_db",
 });
 
