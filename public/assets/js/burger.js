f75fc4fccc1eb964e53696b89ac71704379f72b2
kierajcullen <kierajcullen@gmail.com>
2021-03-23 18:07:14 -0400

    updatinng burger.js

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -14,3 +14,53 @@ $("#burger-btn").on("click", function (event) {
 });
 
 // ajax method with put (look in the cat assignment public/js)
+    $.ajax("/api/burgers/", {
+      type: "POST",
+      data: addBurger,
+    }).then(function () {
+      console.log("Update Burger");
+      location.reload();
+    });
+  });
+  // Add function to devour the burger - put
+  $(".delete").on("click", function (event) {
+    // Prevent default on submit
+    event.preventDefault();
+    console.log("click");
+
+    let id = $(this).attr("data-id");
+
+    // Sending POST request
+    $.ajax("/api/burgers/" + id, {
+      type: "DELETE",
+      //data: addBurger
+    }).then(function () {
+      console.log("Delete Burger");
+
+      location.reload();
+    });
+  });
+  $(".devour").on("click", function (event) {
+    // Prevent default on submit
+    event.preventDefault();
+    console.log("click");
+
+    let id = $(this).attr("data-id");
+    let devouredBurger = $(this).attr("data-devoured");
+    let newBurger = devouredBurger == 0 ? 1 : 0;
+    console.log(devouredBurger, devour);
+    let newStart = {
+      devoured: newBurger,
+    };
+    // Sending POST request
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newStart,
+    }).then(function () {
+      console.log("Delete Burger");
+
+      location.reload();
+      console.log(devouredBurger, devour);
+    });
+  });
+});
